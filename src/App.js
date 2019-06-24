85ee50e89b77fa937a414b80339efc2b749020c3
Sardor Isakov <sisardor@gmail.com>
2019-06-24 16:26:57 -0700

    Update to 5.1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 (() => {
 
   const viewerElement = document.getElementById('viewer');
-  
-  const viewer = new window.PDFTron.WebViewer({
+
+  window.WebViewer({
     path: '../public/lib',
     initialDoc: '../public/files/webviewer-demo-annotated.pdf',
-  }, viewerElement)
-
-  viewerElement.addEventListener('ready', () => {
-    const instance = viewer.getInstance();
+  }, viewerElement).then(instance => {
 
     // Interact with APIs here.
     // See https://www.pdftron.com/documentation/web/guides/basic-functionality for more info/

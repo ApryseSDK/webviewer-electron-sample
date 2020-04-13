d571535ca826a2e70ee80261fa454eda9449e82a
Armando <bollain@gmail.com>
2020-04-13 15:46:48 -0700

    Fix copy script and how WV is required

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -1,12 +1,18 @@
 const fs = require('fs-extra');
+const path = require('path');
 
 const copyFiles = async () => {
+  const sourcePath = path.resolve('./node_modules/@pdftron/webviewer/public');
+  const destPath = path.resolve('./public/lib/');
+
   try {
-    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
+    await fs.copy(sourcePath, destPath);
+    await fs.copy(webViewerMinified, destPath);
+    awa
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
   }
 };
 
-copyFiles();
+copyFiles(); 
